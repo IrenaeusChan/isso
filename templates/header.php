131c0690ef89d1549e436cf630ed785b89affba4
irenaeus <irenaeus.chan@gmail.com>
2017-10-05 14:50:29 -0400

    Initializing ISSO Repository

---
+++ templates/header.php
@@ -0,0 +1,18 @@
+<head>
+	<meta charset="utf-8"/> //Specify the character encoding for the Document; UTF-8 = Unicode
+	<meta name="irenaeus" content="tutorial"/>
+	<title>Irenaeus Secondary Structure Organization v1.0 </title>
+	<meta name="description" content="Tutorial Page for AngularJS">
+
+	<meta http-equiv="X-UA-compatible" content="IE=Edge"/> //Used to support IE9 or IE8
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"/>
+
+	<!-- Adding Styles -->
+	<link href="css/style.css" rel="stylesheet" type="text/css"/> //I think this pulls on the CSS formatting
+
+	<!-- Add JavaScript -->
+	<script src="js/jquery-2.0.3.min.js"></script>
+	<script src="js/angular.min.js"></script>
+	<script src="js/app.js></script>
+	<script src="js/controllers.js"></script>
+</head>
